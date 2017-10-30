067ad95f05777bdc8297de157c6b6d0c33a1deec
binod <mebinodlamichhane123@gmail.com>
2017-10-30 20:29:34 +0545

    sucessfully loaded static resources

---
+++ onlineshopping/src/main/java/com/learn/binod/onlineshopping/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.learn.binod.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView modelAndView=new ModelAndView("page");
+		return modelAndView;
+	}
+
+}
